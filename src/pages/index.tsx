dab7bd3f81c2817d1fe2d707ee5c706d197e62de
Sunny Pacheco <pacheco.sunny@gmail.com>
2023-08-01 14:56:48 -0400

    Hard coded room list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,10 @@ import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
 import Link from 'next/link';
 
 type room = {
-  id: string
+  id: string,
+  name: string
 }
-type chatrooms = room[]
+type chatrooms = Array<room>
 type HomeProps = {
   chatrooms:chatrooms
 }
@@ -12,6 +13,7 @@ type HomeProps = {
 export const getServerSideProps: GetServerSideProps = async () => {
   const res = await fetch(`${process.env.NEXTAUTH_URL}/api/chatrooms`);
   const chatrooms = await res.json();
+  console.log(chatrooms);
   return {
     props: {chatrooms}
   }
@@ -20,20 +22,21 @@ export const getServerSideProps: GetServerSideProps = async () => {
 function Home({ chatrooms }:HomeProps) {
   // Will need to fetch list of chatrooms from database here
   const chatroomList = chatrooms.map(room => {
-    const { id } = room;
+    const { id, name } = room;
     return (
-      <li>
-        <Link href={`/chatroom/${id}`} />
-        <span># of participants</span>
+      <li key={id} className='m-2 text-white'>
+        <p className=' text-xl mb-1 font-semibold'>{name}</p>
+        <p className=' text-xs'># of participants</p>
+        <Link href={`/chatroom/${id}`}className='btn btn-secondary btn-sm rounded-full m-2 text-base-100'>Click to Join</Link>
       </li>
     )
   })
 
   return (
-    <>
-    <h1>hello world</h1>
+    <div className=' flex flex-col font-sans list-none text-center'>
+    <h1 className='text-5xl font-bold tracking-wide m-2 mb-4'>Available Rooms</h1>
       {chatroomList}
-    </>
+    </div>
   )
 }
 
