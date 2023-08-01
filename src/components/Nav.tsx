b831326c57c9b009c6860e1ba0203d19429dd349
Sunny Pacheco <pacheco.sunny@gmail.com>
2023-08-01 14:30:58 -0400

    Navbar and homepage

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,29 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import DesktopNav from "./DesktopNav";
+import MobileNav from "./MobileNav";
+
 const Nav = () => {
+  const { data: session } = useSession();
+
   return (
-    <div>Nav</div>
+    <nav className="navbar justify-between bg-base-100 bg-opacity-20 text-neutral sticky w-full mb-5 p-4 px-6">
+        <Link href='/' className="flex gap-2 items-center justify-center">
+          <Image
+            src='/logo.png'
+            alt='logo'
+            width={45}
+            height={45}
+            className="object-contain"
+          />
+          <p className="text-lg font-bold" >LinguaLive</p>
+        </Link>
+
+      <DesktopNav session={session} />
+      <MobileNav session={session} />
+    </nav>
   )
 }
 
-export default Nav
+export default Nav;
